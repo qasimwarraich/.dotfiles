1e53faac5626ba9cf2ed07924b35a272df9219aa
qasimwarraich <qasim.warraich@gmail.com>
2021-12-10 14:12:53 +0100

    Removed links to icon to fallback on text layout name
    - Added brackets for formatting

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -98,23 +98,24 @@ theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/m
 
 theme.wallpaper = themes_path.."default/background.png"
 
--- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+-- -- You can use your own layout icons like this:
+-- theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
+-- theme.layout_fairv = themes_path.."default/layouts/fairvw.png"rap widget in textbox
+-- theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+-- theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
+-- theme.layout_max = themes_path.."default/layouts/maxw.png"
+-- theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
+-- theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
+-- theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
+-- theme.layout_tile = themes_path.."default/layouts/tilew.png"
+-- theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
+-- theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
+-- theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
+-- theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
+-- theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
+-- theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
+-- theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
