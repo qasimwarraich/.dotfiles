919d20aa5c13c6a716e65fc68e9666dc6f606308
qasimwarraich <qasim.warraich@gmail.com>
2021-12-10 14:15:26 +0100

    Removed taglist squares

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -44,13 +44,13 @@ theme.border_marked = "#91231c"
 --theme.taglist_bg_focus = "#ff0000"
 
 -- Generate taglist squares:
-local taglist_square_size = dpi(4)
-theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
-    taglist_square_size, theme.fg_normal
-)
-theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
-    taglist_square_size, theme.fg_normal
-)
+-- local taglist_square_size = dpi(4)
+-- theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+--     taglist_square_size, theme.fg_normal
+-- )
+-- theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+--     taglist_square_size, theme.fg_normal
+-- )
 theme.taglist_fg_focus="#DAA520"
 -- Variables set for theming notifications:
 -- notification_font
