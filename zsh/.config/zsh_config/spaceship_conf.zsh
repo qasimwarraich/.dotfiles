4102f5b52fb53b8f132e4a7a4cbb13f91e4ea3af
qasimwarraich <qasim.warraich@gmail.com>
2022-02-22 12:06:18 +0100

    Get rid of timestamps and vi mode

--- zsh/.config/zsh_config/spaceship_conf.zsh
+++ zsh/.config/zsh_config/spaceship_conf.zsh
@@ -20,7 +20,7 @@ SPACESHIP_CHAR_SUFFIX=" "
 SPACESHIP_CHAR_COLOR_SUCCESS=white
 
 SPACESHIP_PROMPT_ORDER=(
-  time          # Time stamps section
+  # time          # Time stamps section
   user          # Username section
   host          # Hostname section
   dir           # Current directory section
@@ -34,7 +34,7 @@ SPACESHIP_PROMPT_ORDER=(
   exec_time     # Execution time
   line_sep      # Line break
   battery       # Battery level and status
-  vi_mode       # Vi-mode indicator
+  # vi_mode       # Vi-mode indicator
   jobs          # Background jobs indicator
   exit_code     # Exit code section
   char          # Prompt character
