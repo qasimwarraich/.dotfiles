ca8d3ebbebd7619e2855214c3e642e4eb7195c0f
qasimwarraich <qasim.warraich@gmail.com>
2022-09-29 11:42:00 +0200

    nvim: Seperates cmp config and bindings

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,4 +1,3 @@
-local nnoremap = require('config.keymap').nnoremap
 local nvim_lsp = require('lspconfig')
 local lsp_keymap = require('config.lsp_keymap')
 
@@ -7,8 +6,11 @@ local servers = {
     'vimls', 'html', 'cssls', 'intelephense', 'dockerls', 'metals', 'ansiblels',
     'texlab', 'ltex', 'svelte'
 }
-for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {on_attach = lsp_keymap.attach} end
+for _, lsp in ipairs(servers) do
+    nvim_lsp[lsp].setup {on_attach = lsp_keymap.attach}
+end
 
+-- Custom configs
 require("config.custom_servers.sumneko")
 require("config.custom_servers.go")
 
