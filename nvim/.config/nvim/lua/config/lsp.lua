7b4182faf0fcd21907207f8d69e29610aa7a793f
qasimwarraich <qasim.warraich@gmail.com>
2022-10-08 10:18:26 +0200

    nvim: Added capabilities definition to lsp config
    - This is needed for html cssls

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,13 +1,19 @@
 local nvim_lsp = require('lspconfig')
 local lsp_keymap = require('config.lsp_keymap')
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 local servers = {
     "pyright", "jedi_language_server", "rust_analyzer", "tsserver", "clangd",
     'vimls', 'html', 'cssls', 'intelephense', 'dockerls', 'metals', 'ansiblels',
     'texlab', 'ltex', 'svelte'
 }
 for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {on_attach = lsp_keymap.attach}
+    nvim_lsp[lsp].setup {
+        on_attach = lsp_keymap.attach,
+        capabilities = capabilities
+    }
 end
 
 -- Custom configs
