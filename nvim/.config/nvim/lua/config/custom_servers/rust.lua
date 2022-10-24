56b3afdd1e1f895ae4f85139f0c28d46b1f19ad3
qasimwarraich <qasim.warraich@gmail.com>
2022-10-25 00:17:24 +0200

    nvim: Make rust server config use lsp_keymap

--- nvim/.config/nvim/lua/config/custom_servers/rust.lua
+++ nvim/.config/nvim/lua/config/custom_servers/rust.lua
@@ -1,12 +1,14 @@
 local rt = require("rust-tools")
+local lsp_keymap = require('config.lsp_keymap')
 
 rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>A", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
+    server = {
+        on_attach = function(_, bufnr)
+            lsp_keymap.attach(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>A", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+    },
 })
