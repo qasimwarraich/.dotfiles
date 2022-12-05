41836bffe124fd908f951ff3b5fe3644178e7184
qasimwarraich <qasim.warraich@gmail.com>
2022-12-05 20:12:34 +0100

    nvim(luasnip): Add custom snipped for rust debug print

--- nvim/.config/nvim/lua/config/luasnip.lua
+++ nvim/.config/nvim/lua/config/luasnip.lua
@@ -1,1 +1,15 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local extras = require("luasnip.extras")
+local rep = extras.rep
+
 require("luasnip.loaders.from_vscode").lazy_load()
+
+
+ls.add_snippets("rust", {
+    s({trig= "prd", dscr= "Named debug print {:?}"}, {
+        t("println!(\""), i(1, "var"), t(" = {:?}\", "), rep(1), t(");")
+    })
+})
