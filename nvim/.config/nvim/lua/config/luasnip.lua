7408c967945f034f8dbc4f55ee0a513822f0dca4
qasimwarraich <qasim.warraich@gmail.com>
2022-12-17 22:00:49 +0100

    nvim: Add go err print snippet

--- nvim/.config/nvim/lua/config/luasnip.lua
+++ nvim/.config/nvim/lua/config/luasnip.lua
@@ -3,6 +3,7 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local extras = require("luasnip.extras")
+local fmt = require("luasnip.extras.fmt").fmt
 local rep = extras.rep
 
 require("luasnip.loaders.from_vscode").lazy_load()
@@ -12,4 +13,13 @@ ls.add_snippets("rust", {
     s({ trig = "prd", name = "Debug Print", dscr = "Allows for a named debug print using {:?}" }, {
         t("println!(\""), i(1, "var"), t(" = {:?}\", "), rep(1), t(");")
     })
+})
+
+ls.add_snippets("go", {
+    s({ trig = "erf", name = "Log fatal error", dscr = "Checks if err exists and logs fatally" },
+        fmt([[
+if err != nil {{
+    log.Fatal(err)
+}}]]     , {})
+    )
 })
