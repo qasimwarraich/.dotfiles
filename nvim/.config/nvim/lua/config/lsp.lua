ff15f4117592e35d0417e24c67b2d0505e61b010
qasimwarraich <qasim.warraich@gmail.com>
2023-02-22 21:57:58 +0100

    nvim(lsp): Add eslint

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -7,7 +7,7 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 local servers = {
     "pyright", "jedi_language_server", "tsserver", "clangd",
     'vimls', 'html', 'cssls', 'intelephense', 'dockerls', 'metals', 'ansiblels',
-    'texlab', 'ltex', 'svelte', 'tailwindcss', 'denols'
+    'texlab', 'ltex', 'svelte', 'tailwindcss', 'denols', 'eslint'
 }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
