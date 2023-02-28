2de055f3dc6f3821edd3a3ae23858b7616c5d4ca
qasimwarraich <qasim.warraich@gmail.com>
2023-02-28 20:23:35 +0100

    nvim: Add fifth harpoon binding 🖐

--- nvim/.config/nvim/after/plugin/keymaps/harpoon.lua
+++ nvim/.config/nvim/after/plugin/keymaps/harpoon.lua
@@ -7,3 +7,4 @@ vim.keymap.set("n", "<leader>1", function() require("harpoon.ui").nav_file(1) en
 vim.keymap.set("n", "<leader>2", function() require("harpoon.ui").nav_file(2) end, silent)
 vim.keymap.set("n", "<leader>3", function() require("harpoon.ui").nav_file(3) end, silent)
 vim.keymap.set("n", "<leader>4", function() require("harpoon.ui").nav_file(4) end, silent)
+vim.keymap.set("n", "<leader>5", function() require("harpoon.ui").nav_file(4) end, silent)
