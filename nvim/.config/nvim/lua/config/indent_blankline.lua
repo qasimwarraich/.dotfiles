37553ed14883ae1447366ab6ef0ac5b24d024287
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2023-10-21 21:29:21 +0200

    nvim: Update indent_blankline config to v3

--- nvim/.config/nvim/lua/config/indent_blankline.lua
+++ nvim/.config/nvim/lua/config/indent_blankline.lua
@@ -1,6 +1,7 @@
-require("indent_blankline").setup({
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-	disable_with_nolist = true,
+require("ibl").setup({
+    enabled = false,
+    whitespace = {
+        highlight = { "Function", "Label" },
+        remove_blankline_trail = true,
+    },
 })
