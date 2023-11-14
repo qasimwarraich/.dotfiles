fb92a0565d7268cd11bd9520dc397c39a78eab76
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2023-11-14 11:58:56 +0100

    nvim: Set inlay hints to `eol`

--- nvim/.config/nvim/lua/config/custom_servers/go.lua
+++ nvim/.config/nvim/lua/config/custom_servers/go.lua
@@ -1,8 +1,8 @@
 local attach = require("config.lsp_keymap").attach
 
 require("go").setup({
-	goimport = "goimports", -- if set to 'gopls' will use golsp format
-	gofmt = "gopls", -- if set to gopls will use golsp format
+	goimport = "gopls", -- if set to 'gopls' will use golsp format
+	gofmt = "gofumpt", -- if set to gopls will use golsp format
 	max_line_len = 120,
 	tag_transform = false,
 	test_dir = "",
@@ -10,11 +10,14 @@ require("go").setup({
 	lsp_cfg = true, -- false: use your own lspconfig
 	lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
 	lsp_on_attach = attach, -- use on_attach from go.nvim
-	lsp_diag_signs = true,
 	test_runner = "go",
 	dap_debug = true,
 	dap_debug_keymap = false,
 	run_in_floaterm = true,
+	lsp_inlay_hints = {
+        enable = true,
+		style = 'eol',
+	},
 })
 
 -- require('dap-go').setup()
