45d5148bde54dfdde9e6b7feb3790235d052306a
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2023-12-19 17:07:58 +0100

    nvim: Update telescope ignore pattern

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -4,6 +4,6 @@ require("telescope").setup({
 			require("telescope.themes").get_dropdown({}),
 		},
 	},
-    file_ignore_patterns = {"node_modules", "bundle.js", "bundle.map.js"},
+    file_ignore_patterns = {"node_modules", "bundle.js", "bundle.js.map"},
 })
 require("telescope").load_extension("ui-select")
