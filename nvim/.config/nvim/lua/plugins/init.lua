4fed0fed94b38f5b481315602457d65b0a62467e
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2024-10-18 10:37:37 +0200

    nvim: Update `go.nvim` configuration

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -7,9 +7,22 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			"ray-x/go.nvim",
 			"vigoux/ltex-ls.nvim",
 		},
+	},
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("config.custom_servers.go")
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 	},
 	{
 		"mrcjkb/rustaceanvim",
