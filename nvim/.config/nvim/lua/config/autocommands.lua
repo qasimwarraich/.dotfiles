a0e1ea27c72174168df8e5d64b162eced4aa79db
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2025-04-10 11:18:44 +0200

    nvim: Remove unneeded autocommands

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -7,12 +7,3 @@ autocmd("TextYankPost", {
 		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 40 })
 	end,
 })
-
--- Run gofmt + goimport on save
--- vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
-
--- Run Neoformat on svelte
-vim.api.nvim_exec([[ autocmd BufWritePre *.svelte :Neoformat<CR>]], false)
-
--- Lsp Lightbulb
-vim.cmd([[autocmd CursorHold,CursorHoldI * lua require('nvim-lightbulb').update_lightbulb()]])
