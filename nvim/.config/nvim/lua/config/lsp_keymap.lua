8cb6b2bd7883d4297c65271d440e2f4647e45a1d
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2025-04-10 11:22:28 +0200

    nvim: Remove deprecations and clean up lsp related keymaps

--- nvim/.config/nvim/lua/config/lsp_keymap.lua
+++ nvim/.config/nvim/lua/config/lsp_keymap.lua
@@ -2,12 +2,10 @@ local M = {}
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<space>E", vim.diagnostic.open_float, opts)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+    -- vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
@@ -23,14 +21,15 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
 	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("v", "<space>ca", vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 	vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
 
-	local active_clients = vim.lsp.get_active_clients()
+	local active_clients = vim.lsp.get_clients()
 	if client.name == "denols" then
 		for _, client_ in pairs(active_clients) do
 			if client_.name == "tsserver" then
-				client_.stop()
+				client.stop()
 			end
 		end
 	elseif client.name == "tsserver" then
