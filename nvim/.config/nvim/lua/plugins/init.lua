b65edc14488f1f57a7ab4346cd85acc214793629
qasimwarraich <37380474+qasimwarraich@users.noreply.github.com>
2025-11-05 07:44:29 +0100

    nvim: add `remote-nvim.nvim`

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -170,6 +170,16 @@ return {
 	{
 		"chrisgrieser/nvim-lsp-endhints",
 		event = "LspAttach",
-		opts = {}, 
+		opts = {},
+	},
+	{
+		"amitds1997/remote-nvim.nvim",
+		version = "*", -- Pin to GitHub releases
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- For standard functions
+			"MunifTanjim/nui.nvim", -- To build the plugin UI
+			"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+		},
+		config = true,
 	},
 }
